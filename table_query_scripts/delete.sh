f3c855d2c77bf97911586ab926818c90ae68fb2c
hossamesaaa <hossamesaa2019@gmail.com>
2023-08-05 21:31:25 +0200

    adding delete feature

---
+++ table_query_scripts/delete.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+
+
+function deleteTable (){
+
+ local table_name
+ 
+ read -p "Enter tablename "  table_name
+ 
+ if [[ -f ${table_name}* ]];then 
+ 
+  select var in "all" "record" "Exit"
+  do
+     if [[ $REPLY =~ ^[1-3]$ && -n $REPLY ]] ;then
+       case $REPLY in
+
+         1)
+            rm -r ./${table_name}_data.txt
+
+            if [[ $? -eq 0 ]];then
+             echo "the table has been deleted successfully"
+            else
+              echo "there is an error to delete the table"
+            fi
+             ;;
+
+          2)
+            read -p "Enter the key :" key
+
+            sed -i '/$key/d'  ./${table_name}_data.txt
+            if [[ $? -eq 0 ]];then
+             echo "record has been deleted successfully" 
+            else
+              echo "there is an error to delete record"          
+            fi 
+              ;;
+     
+         3)
+             exit
+             ;;
+
+      esac
+    else
+       echo "please enter valid number"
+    fi
+
+   done
+  
+  else
+     echo "the table doesn't exist"
+  fi
+
+}
+
