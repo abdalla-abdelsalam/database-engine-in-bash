48b191a85bd4cef9272a1695018ae863d712a8f8
abdalla-abdelsalam <abdalla.abdelsalamm@gmail.com>
2023-08-05 22:20:40 +0300

    fix #10

--- table_query_scripts/create.sh
+++ table_query_scripts/create.sh
@@ -2,9 +2,7 @@
 
 create_schema() {
   local database_name=$1
-  shift
-  local table_name=$1
-  shift
+  read -p "enter table name: " table_name
 
   local schema_file="databases/${database_name}/table_definitions/${table_name}_schema.txt"
 
@@ -14,15 +12,19 @@ create_schema() {
     return 1
   fi
 
+  touch ${schema_file}
 
+  while true; do
+    read -p "Enter column name (or 'done' to finish): " column_name
+    if [[ "$column_name" == "done" ]]; then
+      break
+    fi
 
-  while (( $# >= 2 )); do
-    local column_name=$1
-    local data_type=$2
+    read -p "Enter data type (e.g., integer, text, float): " data_type
 
     echo "$column_name:$data_type" >> "$schema_file"
-    shift 2
   done
+
 
   echo "Schema for '$table_name' created successfully in '$schema_file'."
 }
